6a1551b2c01646ee8578f04bf1e31ce3c730acf9
GunstarKarrot <gunstarkarrot@gmail.com>
2024-12-07 14:45:03 -0500

    Added csharp starting point, nothing useful yet

---
+++ cSharp/dayOne/DayOne.cs
@@ -0,0 +1,15 @@
+import System;
+
+public class Program {
+    public static parseArguments(String[] args) {
+        if (args.Length == 0) {
+            Console.WriteLine("No arguments provided");
+        } else {
+            Console.WriteLine("Arguments provided");
+        }
+    }
+    public static void Main(String[] args) {
+
+    }
+
+}
