4f52669c2450a40f8e08b5511f6e23c1518168fc
GunstarKarrot <gunstarkarrot@gmail.com>
2024-12-07 18:53:16 -0500

    Second star for python for day one

--- python/day_one/day_one.py
+++ python/day_one/day_one.py
@@ -34,6 +34,40 @@ def readFile(inputPath):
                 print("Error: Could not convert to int or out of range")
         return listA, listB
 
+def buildCountMap(list):
+    """Build a count map of the list
+
+    Build a count map of the list by iterating through the list
+    and counting the number of times each element appears
+
+    :param list: List of integers
+    :return countMap: Dictionary containing the count of each element
+    """
+    countMap = {}
+    for element in list:
+        if element in countMap:
+            countMap[element] += 1
+        else:
+            countMap[element] = 1
+    return countMap
+
+def findSum(list, map):
+    """Find the sum of elements multiplied by their count
+
+    Find the sum of the elements in the list multiplied by their count
+    by iterating through the list and multiplying the element by its count
+    in the map
+
+    :param list: List of integers
+    :param map: Dictionary containing the count of each element
+    :return sum: Sum of the elements multiplied by their count
+    """
+    sum = 0
+    for element in list:
+        if element in map:
+            sum += element * map[element]
+    return sum
+
 def main():
     # Process command line arguments
     args = parse_args()
@@ -62,6 +96,12 @@ def main():
     for i in range(len(listA)):
         totalDistance += abs(listA[i] - listB[i])
     print(f"Total distance between the two lists: {totalDistance}")
+
+
+    countMap = buildCountMap(listB)
+    sum = findSum(listA, countMap)
+
+    print(f"Sum of the elements in list A multiplied by their count in list B: {sum}")
 
 if __name__ == "__main__":
     # Main entry point
