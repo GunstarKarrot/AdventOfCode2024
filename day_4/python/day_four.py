4e8eb41117aeead1be944590919e944a345b96c1
GunstarKarrot <gunstarkarrot@gmail.com>
2024-12-14 15:36:44 -0500

    Fixed Day4

--- day_4/python/day_four.py
+++ day_4/python/day_four.py
@@ -13,7 +13,7 @@ def parse_args():
 
 def read_input(input_file):
     """Read input file as a 2d grid of chars
-    
+
     Read the input file and turn it into a 2d array grid of characters
 
     :param input_file: Path to the input file
@@ -26,28 +26,64 @@ def read_input(input_file):
     return grid
 
 def check_horizontal(grid, row, col):
+    """Check for the word MAS horizontally
+
+    Check for the word "XMAS" after receiving the index of "X".
+    Check horizontally in the grid, both forwards and backwards.
+    Check if there is enough space to check for the word in
+    both directions. Then check if the word is present in both directions.
+
+    :param grid: 2d grid of characters
+    :param row: row index
+    :param col: column index
+    :return total: total number of "MAS" found
+    """
     total = 0
     if not col + 3 >= len(grid[row]):
         if grid[row][col + 1] == "M" and grid[row][col + 2] == "A" and grid[row][col + 3] == "S":
             total += 1
-    if col - 3 < 0:
+    if not col - 3 < 0:
         if grid[row][col - 1] == "M" and grid[row][col - 2] == "A" and grid[row][col - 3] == "S":
             total += 1
 
     return total
 
 def check_vertical(grid, row, col):
+    """Check for the word MAS vertially
+
+    Check for the word "XMAS" after receiving the index of "X".
+    Check veritcally in the grid, both forwards and backwards.
+    Check if there is enough space to check for the word in
+    both directions. Then check if the word is present in both directions.
+
+    :param grid: 2d grid of characters
+    :param row: row index
+    :param col: column index
+    :return total: total number of "MAS" found
+    """
     total = 0
     if not row + 3 >= len(grid):
         if grid[row + 1][col] == "M" and grid[row + 2][col] == "A" and grid[row + 3][col] == "S":
             total += 1
-    if row - 3 < 0:
+    if not row - 3 < 0:
         if grid[row - 1][col] == "M" and grid[row - 2][col] == "A" and grid[row - 3][col] == "S":
             total += 1
-    
+
     return total
 
 def check_diagonal(grid, row, col):
+    """Check for the word MAS horizontally
+
+    Check for the word "XMAS" after receiving the index of "X".
+    Check horizontally in the grid, both forwards and backwards.
+    Check if there is enough space to check for the word in
+    both directions. Then check if the word is present in both directions.
+
+    :param grid: 2d grid of characters
+    :param row: row index
+    :param col: column index
+    :return total: total number of "MAS" found
+    """
     total = 0
     if not row + 3 >= len(grid) and not col + 3 >= len(grid[row]):
         if grid[row + 1][col + 1] == "M" and grid[row + 2][col + 2] == "A" and grid[row + 3][col + 3] == "S":
@@ -61,7 +97,7 @@ def check_diagonal(grid, row, col):
     if not row - 3 < 0 and not col + 3 >= len(grid[row]):
         if grid[row - 1][col + 1] == "M" and grid[row - 2][col + 2] == "A" and grid[row - 3][col + 3] == "S":
             total += 1
-            
+
     return total
 
 def main():
@@ -70,13 +106,13 @@ def main():
 
     totalFinds = 0
     for i in range(len(grid)):
-        for j in range(len(grid[i])):   
+        for j in range(len(grid[i])):
             if grid[i][j] == "X":
                 h = check_horizontal(grid, i, j)
                 v = check_vertical(grid, i, j)
                 d = check_diagonal(grid, i, j)
                 totalFinds += h + v + d
-    
+
     print(totalFinds)
 
 
